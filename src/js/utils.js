210f9703661b0547d8c16c91fee5df942f816846
botelho <botelho@users.noreply.github.com>
2021-08-23 10:12:59 +0100

    Update getMousePos function in utils.js
    The cursor y-position is now correctly calculated for the case when the page is scrolled.

--- src/js/utils.js
+++ src/js/utils.js
@@ -13,20 +13,11 @@ const calcWinsize = () => {
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 // Gets the mouse position
-const getMousePos = (e) => {
-    let posx = 0;
-    let posy = 0;
-    if (!e) e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY)    {
-        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    
-    return { x : posx, y : posy }
+const getMousePos = e => {
+    return { 
+        x : e.clientX, 
+        y : e.clientY 
+    };
 };
 
 // Preload images
